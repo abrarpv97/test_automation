0ae9261e9fed034fd7ebde2c4b1398bb479ebb2f
abrarpv <abrarpv97@gmail.com>
2020-08-04 19:07:28 +0530

    make journal entry from salary slip

--- test_automation/public/js/salary_slip.js
+++ test_automation/public/js/salary_slip.js
@@ -1,18 +1,24 @@
 frappe.ui.form.on('Salary Slip', {
     refresh: function(frm) {
 		if (frm.doc.docstatus == 1) {
-				frm.add_custom_button(__("Make Payment"),
-						frm.events.make_journal(frm)
-				);
+				frm.add_custom_button(__("Make Payment"),function(){
+					frm.events.make_journal(frm);
+				});
         }
 	},
 	
     make_journal: function (frm) {
 		return frappe.call({
-			doc: frm.doc,
-			method: 'make_journal_entry',
+			method: "test_automation.test_automation.salary_slip.salary_slip.make_journal_entry",
+			args: { 
+				source_name: frm.doc.name
+			},
 			callback: function(r) {
-				alert('success');
+				frappe.msgprint({
+					title: __('Notification'),
+					indicator: 'green',
+					message: __('Journal entries created for this salary slip')
+				});
 			}
 		})
 	}
